34e75210efffd7cad88d43f379eef3fd1d01c3cd
Daniel Kennedy <danielrkennedy03@gmail.com>
2026-03-13 21:26:22 +0000

    feat(common): nozzle grid, opposite indexing to original

--- src/MOC_2D/common/Grid.h
+++ src/MOC_2D/common/Grid.h
@@ -19,4 +19,34 @@ struct GridPoint {
     quantity<mp_units::si::kelvin>                                  temperature{};         // T
     quantity<mp_units::si::kilogram / cubic(mp_units::si::metre)>   density{};             // rho
 };
+
+struct NozzleGrid {
+    std::vector<std::vector<GridPoint>> points;
+
+    static constexpr size_t max_rrc = 1000;
+
+    void add_rrc(size_t radial_count) {
+        if (points.size() > max_rrc) {
+            throw std::runtime_error("RRC count exceeded maximum");
+        }
+        points.emplace_back(radial_count);
+    }
+
+    std::vector<GridPoint> wall_contour() const {
+        std::vector<GridPoint> wall;
+        wall.reserve(points.size());
+        for (const auto& rrc : points)
+            wall.push_back(rrc[0]);
+        return wall;
+    }
+
+    std::span<const GridPoint> rrc(size_t j) const {
+        return points[j];
+    }
+
+    const GridPoint& centreline_at(size_t j) const {
+        return points[j].back();
+    }
+
+};
 }
